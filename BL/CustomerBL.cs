fa3356f30b5cb8f732d2a01813f29c0885e80368
Kira <kirosa1994@gmail.com>
2022-02-13 11:07:00 -0500

    Changed

--- BL/CustomerBL.cs
+++ BL/CustomerBL.cs
@@ -13,18 +13,12 @@ namespace BL
 
     public Customer AddCustomer(Customer p_cus)
     {
-      List<Customer> _listCustomers = _repo.GetALlCustomers();
-
-      for (int i = 0; i < _listCustomers.Count(); i++)
+      if (GetALlCustomers().Any(p => p.Name == p_cus.Name))
       {
-        if (_listCustomers[i].Name == p_cus.Name)
-        {
-          throw new Exception("Cannot add new customer due to this name is already in the database!");
-        }
+        throw new Exception("Cannot add new customer due to this name is already in the database!");
       }
 
       return _repo.AddCustomer(p_cus);
-      // if (_listCustomers.All(cus => cus.Name != p_cus.Name)) {}
     }
 
     public List<Customer> GetALlCustomers()
